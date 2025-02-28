9e483199240d96ec10600c68a9564dec4a02d5d2
Developer Experience team at Ignite <hello@ignite.com>
2025-02-28 12:05:42 +0800

    Initialized with Ignite CLI

---
+++ problem-5/crude-chain/ledger/x/ledger/module/genesis_test.go
@@ -0,0 +1,30 @@
+package ledger_test
+
+import (
+	"testing"
+
+	keepertest "ledger/testutil/keeper"
+	"ledger/testutil/nullify"
+	ledger "ledger/x/ledger/module"
+	"ledger/x/ledger/types"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.LedgerKeeper(t)
+	ledger.InitGenesis(ctx, k, genesisState)
+	got := ledger.ExportGenesis(ctx, k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
